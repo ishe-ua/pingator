e539fbf390f9e61af5b686b86a959307666fdac7
ishe_ua <in.shevkun@gmail.com>
2018-04-14 16:39:00 +0300

    Excepton if lost local file

--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -2,3 +2,10 @@
 
 Rails.application.config.i18n.load_path +=
   Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+
+if Rails.env.development? || Rails.env.test?
+  # TODO: check working
+  # Raises error for missing translations
+  Rails.application.config.action_view
+       .raise_on_missing_translations = true
+end
