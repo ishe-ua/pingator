fe75be9e65be975d2f82fc0abafd05e593611e0d
ishe_ua <in.shevkun@gmail.com>
2018-05-13 13:38:53 +0300

    Show account in settings

--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -1,11 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.config.i18n.load_path +=
-  Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
 
-if Rails.env.development? || Rails.env.test?
-  # TODO: check working
-  # Raises error for missing translations
-  Rails.application.config.action_view
-       .raise_on_missing_translations = true
-end
+Rails.application.config.action_view
+  .raise_on_missing_translations = true
