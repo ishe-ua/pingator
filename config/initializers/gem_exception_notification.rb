37c8de870ce21566dc7c8913b98896c7774d79b4
ishe_ua <in.shevkun@gmail.com>
2018-05-19 10:26:49 +0300

    Dry

--- config/initializers/gem_exception_notification.rb
+++ config/initializers/gem_exception_notification.rb
@@ -5,9 +5,7 @@ require 'exception_notification/rails'
 ExceptionNotification.configure do |config|
   # Adds a condition to decide when an exception must be ignored or not.
   # The ignore_if method can be invoked multiple times to add extra conditions.
-  config.ignore_if do |_exception, _options|
-    Rails.env.development? || Rails.env.test?
-  end
+  config.ignore_if { |_exception, _options| !prod? }
 
   # Email notifier sends notifications by email.
   config.add_notifier :email,
