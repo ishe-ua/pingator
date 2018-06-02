9556e2457d7b583dd9ea82295a713b5fddb45000
ishe_ua <in.shevkun@gmail.com>
2018-06-02 09:52:32 +0300

    Opt

--- config/initializers/gem_vcr.rb
+++ config/initializers/gem_vcr.rb
@@ -4,5 +4,6 @@ if Rails.env.test?
   VCR.configure do |config|
     config.cassette_library_dir = 'tmp/cassettes'
     config.hook_into :faraday
+    raise 'Faraday not defined' unless defined?(Faraday)
   end
 end
