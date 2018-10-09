b726e1b361945a758606ca1815b4c3ac7b6a8f7a
ishe_ua <in.shevkun@gmail.com>
2018-10-09 12:01:21 +0300

    Opt

--- app/models/concerns/color.rb
+++ app/models/concerns/color.rb
@@ -15,9 +15,9 @@ module Color
 
   LIST = [GREEN, YELLOW, RED].freeze
 
-  GREEN_CODES  = (200...300).to_a # http status codes
-  YELLOW_CODES = (300...400).to_a
-  RED_CODES    = (400...600).to_a + [Code::BAD_CONNECTION]
+  GREEN_CODES  = (200..299).to_a # http status codes
+  YELLOW_CODES = (300..399).to_a
+  RED_CODES    = (400..599).to_a + [Code::BAD_CONNECTION]
 
   included do
     scope :greens,  -> { where code: GREEN_CODES  }
