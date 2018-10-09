1f5842da1129105ae5da66810e3edbe58729ea6f
ishe_ua <in.shevkun@gmail.com>
2018-10-09 21:34:53 +0300

    Test for target current_status

--- test/helpers/targets_helper_test.rb
+++ test/helpers/targets_helper_test.rb
@@ -3,7 +3,20 @@
 require 'test_helper'
 
 class TargetsHelperTest < ActionView::TestCase
-  test 'truth' do
-    skip
+  test 'current_status locales presence' do
+    assert_key_presence 'locked'
+
+    assert_key_presence 'verification.wait'
+    assert_key_presence 'verification.verified'
+    assert_key_presence 'verification.not_verified'
+
+    assert_key_presence 'status.success'
+    assert_key_presence 'status.fail'
+  end
+
+  private
+
+  def assert_key_presence(key)
+    assert I18n.exists?("targets_helper.current_status.#{key}")
   end
 end
